9209a68b0de8f5a17cc9b885f10a65cdeedbc704
Samuli Ulmanen <sulmanen@thinglink.com>
2016-04-04 12:20:18 +0300

    plot to different figures

--- compareMatchers.m
+++ compareMatchers.m
@@ -2,8 +2,12 @@
 
 % dct
 plotHamming('../practical-object-instance-recognition/data/paintings/t00*.jpg','dct', db);
+figure;
+
 plotHamming('../practical-object-instance-recognition/data/paintings/h00*.jpg','dct', db);
+figure;
 
 % simple
 plotHamming('../practical-object-instance-recognition/data/paintings/t00*.jpg','simple', db);
+figure;
 plotHamming('../practical-object-instance-recognition/data/paintings/h00*.jpg','simple', db);
