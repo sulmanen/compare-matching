418262d471957bb9227d82125ef47e2e14e9d392
Samuli Ulmanen <sulmanen@thinglink.com>
2016-04-05 22:14:16 +0300

    better docs

--- matchLSIR.m
+++ matchLSIR.m
@@ -12,8 +12,7 @@ end
 [scores, perm] = sort(scores, 'descend') ;
 ii = perm(1); 
 fileIdx = strrep(strrep(strrep(file, 't0', '0'),'h0', '0'), '../practical-object-instance-recognition/data/paintings/','');
-% if the returned db index and the file index plus 3 are equal we have a correct match 
-% it's dirty but an easy way to verify for a correct match
+%% if paths match, we have a correct match
 match.Correct = strcmp(imdb.images.name(ii), fileIdx);
 match.Type = 'lsir';
 match.File = file;
